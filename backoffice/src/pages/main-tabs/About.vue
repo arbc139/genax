b32e21df121b911d60d299155df69938c2030b47
bcb225 <bcb225@gmail.com>
2017-11-16 00:07:27 +0900

    Update About.vue

--- backoffice/src/pages/main-tabs/About.vue
+++ backoffice/src/pages/main-tabs/About.vue
@@ -1,8 +1,10 @@
 <template>
+ <md-whiteframe>
   <div>
     <h1>Welcome to GENAX</h1>
     <p>GENAX uses the keywords you enter to dynamically find genes associated with your keywords.</p>
   </div>
+ </md-whiteframe>
 </template>
 
 <script>
