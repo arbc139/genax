38e65c8fbb4eec287a9d994c78ea62f883717913
arbc139 <arbc139@gmail.com>
2017-11-15 17:51:19 +0900

    Changes select to radio buttons in Parameter setting page.

--- backoffice/src/pages/Parameter.vue
+++ backoffice/src/pages/Parameter.vue
@@ -24,31 +24,65 @@
         {{ $t('parameter.errorMessages.MAX_PVAL') }}
       </span>
     </md-input-container>
-    <md-input-container
-      :class="{
-        'input-container': true,
-        'md-input-invalid': !conditions.COOC_EM,
-      }"
-    >
-      <label>{{ $t('parameter.labels.COOC_EM') }}</label>
-      <md-select
-        name="parameter-COOCEM"
-        id="parameter-COOCEM"
+    <label class="radio-label">{{ $t('parameter.labels.COOC_EM') }}</label>
+    <md-layout md-column md-align="start">
+      <md-radio
         v-model="parameters.COOC_EM"
+        id="cooc-em-support"
+        name="cooc-em-support"
+        :md-value="0"
       >
-        <md-option :value="0">{{ $t('parameter.selects.COOC_EM.support') }}</md-option>
-        <md-option :value="1">{{ $t('parameter.selects.COOC_EM.logPvalue') }}</md-option>
-        <md-option :value="2">{{ $t('parameter.selects.COOC_EM.oddRatio') }}</md-option>
-        <md-option :value="3">{{ $t('parameter.selects.COOC_EM.relativeRisk') }}</md-option>
-        <md-option :value="4">{{ $t('parameter.selects.COOC_EM.lift') }}</md-option>
-        <md-option :value="5">{{ $t('parameter.selects.COOC_EM.chiSquaredStatistic') }}</md-option>
-        <md-option :value="6">{{ $t('parameter.selects.COOC_EM.phiCoefficient') }}</md-option>
-        <md-option :value="7">{{ $t('parameter.selects.COOC_EM.contingencyCoefficient') }}</md-option>
-      </md-select>
-      <span class="md-error" v-show="!conditions.COOC_EM">
-        {{ $t('parameter.errorMessages.COOC_EM') }}
-      </span>
-    </md-input-container>
+        {{ $t('parameter.selects.COOC_EM.support') }}
+      </md-radio>
+      <md-radio
+        v-model="parameters.COOC_EM"
+        id="cooc-em-logPvalue"
+        name="cooc-em-logPvalue"
+        :md-value="1"
+      >
+        {{ $t('parameter.selects.COOC_EM.logPvalue') }}
+      </md-radio>
+      <md-radio
+        v-model="parameters.COOC_EM"
+        id="cooc-em-oddRatio"
+        name="cooc-em-oddRatio"
+        :md-value="2"
+      >
+        {{ $t('parameter.selects.COOC_EM.oddRatio') }}
+      </md-radio>
+      <md-radio
+        v-model="parameters.COOC_EM"
+        id="cooc-em-lift"
+        name="cooc-em-lift"
+        :md-value="4"
+      >
+        {{ $t('parameter.selects.COOC_EM.lift') }}
+      </md-radio>
+      <md-radio
+        v-model="parameters.COOC_EM"
+        id="cooc-em-chiSquaredStatistic"
+        name="cooc-em-chiSquaredStatistic"
+        :md-value="5"
+      >
+        {{ $t('parameter.selects.COOC_EM.chiSquaredStatistic') }}
+      </md-radio>
+      <md-radio
+        v-model="parameters.COOC_EM"
+        id="cooc-em-phiCoefficient"
+        name="cooc-em-phiCoefficient"
+        :md-value="6"
+      >
+        {{ $t('parameter.selects.COOC_EM.phiCoefficient') }}
+      </md-radio>
+      <md-radio
+        v-model="parameters.COOC_EM"
+        id="cooc-em-contingencyCoefficient"
+        name="cooc-em-contingencyCoefficient"
+        :md-value="7"
+      >
+        {{ $t('parameter.selects.COOC_EM.contingencyCoefficient') }}
+      </md-radio>
+    </md-layout>
   </md-layout>
 </template>
 
@@ -81,6 +115,11 @@ export default Vue.component('parameter', {
 }
 
 .parameters-label {
+  font-size: 12px;
+  color: rgba(0, 0, 0, 0.54);
+}
+
+.radio-label {
   font-size: 12px;
   color: rgba(0, 0, 0, 0.54);
 }
