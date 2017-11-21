72d140f088b70d2f7a7daaaa3fb63f9f542b3e8a
arbc139 <arbc139@gmail.com>
2017-11-21 21:41:10 +0900

    Improves layout configuration in Performance page.

--- backoffice/src/pages/main-tabs/Performance.vue
+++ backoffice/src/pages/main-tabs/Performance.vue
@@ -1,19 +1,37 @@
 <template>
   <div class="root">
-      <div class="left-frame">
-        <md-whiteframe md-elevation="2">
+    <md-whiteframe class="root-paper" md-elevation="2">
+      <div class="title-container">
+        <h1>Performance Test</h1>
+        <span class="md-body-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span>
+      </div>
+      <md-whiteframe class="chart-container" md-elevation="2">
+        <div class="chart-instruction">
+          <span class="md-title chart-title">Number of Paper</span>
+          <span class="md-body-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span>
+        </div>
+        <md-whiteframe class="chart-wrapper" md-elevation="2">
           <scatter-chart
+            class="chart"
             :data="numberOfPaper.data"
             :options="numberOfPaper.options"
           />
         </md-whiteframe>
-        <md-whiteframe md-elevation="2">
+      </md-whiteframe>
+      <md-whiteframe class="chart-container" md-elevation="2">
+        <div class="chart-instruction">
+          <span class="md-title chart-title">Support Cut</span>
+          <span class="md-body-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span>
+        </div>
+        <md-whiteframe class="chart-wrapper" md-elevation="2">
           <scatter-chart
+            class="chart"
             :data="absoluteLog.data"
             :options="absoluteLog.options"
           />
         </md-whiteframe>
-    </div>
+      </md-whiteframe>
+    </md-whiteframe>
   </div>
 </template>
 
@@ -36,29 +54,50 @@ export default {
 
 <style scoped>
 .root {
-  display: flex;
-  flex-direction: row;
   overflow: auto;
+  padding: 16px;
 }
 
-.left-frame {
+.root-paper {
   display: flex;
   flex-direction: column;
-  width: 50%;
   height: fit-content;
   padding: 16px;
 }
 
-.left-frame .md-whiteframe {
+.title-container {
+  display: flex;
+  flex-direction: column;
   margin-bottom: 16px;
 }
 
-.left-frame .md-whiteframe:last-child {
+.chart-container {
+  display: flex;
+  flex-direction: row;
+  padding: 16px;
+  margin-bottom: 16px;
+}
+
+.chart-container:last-child {
   margin-bottom: 0;
 }
 
-.bcbTemp {
-  margin: 15px;
-  padding: 15px;
+.chart-instruction {
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  margin-right: 16px;
+}
+
+.chart-title {
+  margin-bottom: 8px;
+}
+
+.chart-wrapper {
+  flex: 1;
+}
+
+.chart {
+  margin: 16px;
 }
 </style>
