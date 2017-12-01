f212dc106659f3db1158b6fff3e4432a890d932b
bcb225 <bcb225@gmail.com>
2017-12-01 23:40:14 +0900

    Update admmission.py

--- gena/admmission.py
+++ gena/admmission.py
@@ -5,9 +5,10 @@ import smtplib
 from email.mime.text import MIMEText
 from lxml import etree
 
-url = 'https://www2.yonsei.ac.kr/entrance/2018/medi/pass2018_med_1st/pass.asp'
+#url = 'https://www2.yonsei.ac.kr/entrance/2018/medi/pass2018_med_1st/pass.asp'
 #url = 'https://www2.yonsei.ac.kr/entrance/2017/medi/pass2017_med_1st/pass.asp'
-url = 'http://admission.yonsei.ac.kr/seoul/admission/html/counsel/notice.asp?s_type=TYPE67'
+#url = 'http://admission.yonsei.ac.kr/seoul/admission/html/counsel/notice.asp?s_type=TYPE67'
+url = 'http://admission.yonsei.ac.kr/seoul/admission/html/medical/pass.asp'
 yonsei = requests.get(url)
 old_html = yonsei.text
 while True:
@@ -19,6 +20,7 @@ while True:
 		print("Not Yet")
 	else:
 		print("Updated")
+		old_html = new_html
 		smtp = smtplib.SMTP('smtp.gmail.com', 587)
 	        smtp.ehlo()      # say Hello
 	        smtp.starttls()  # TLS
